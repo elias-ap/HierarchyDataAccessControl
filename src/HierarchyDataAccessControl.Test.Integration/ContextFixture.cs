db3a31c7dcbb8853e81e9c9b546f3f1650ce98e2
elias-ap <elias.ap.pro@gmail.com>
2025-06-19 16:49:39 -0300

    add context collection fixture

---
+++ src/HierarchyDataAccessControl.Test.Integration/ContextFixture.cs
@@ -0,0 +1,16 @@
+﻿using HierarchyDataAccessControl.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HierarchyDataAccessControl.Test.Integration
+{
+    public class ContextFixture
+    {
+        public readonly HierarchyDataAccessContext context;
+        public ContextFixture()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<HierarchyDataAccessContext>();
+            optionBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HierarchyDataAccessControl");
+            context = new HierarchyDataAccessContext(optionBuilder.Options);
+        }
+    }
+}
--- src/HierarchyDataAccessControl.Test.Integration/HierarchyDataAccessContextGetAllTypes.cs
+++ src/HierarchyDataAccessControl.Test.Integration/HierarchyDataAccessContextGetAllTypes.cs
@@ -1,20 +1,23 @@
 using HierarchyDataAccessControl.Data;
 using HierarchyDataAccessControl.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
 
 namespace HierarchyDataAccessControl.Test.Integration
 {
+    [Collection(nameof(ContextCollectionFixture))]
     public class HierarchyDataAccessContextGetAllTypes
     {
+        private readonly HierarchyDataAccessContext context;
+        public HierarchyDataAccessContextGetAllTypes(ContextFixture contextFixture)
+        {
+            context = contextFixture.context;
+        }
+
         [Fact]
         public void ReturnAllTypesWhenGetAllTypesFromDatabase()
         {
-            // Arrange
-            var optionBuilder = new DbContextOptionsBuilder<HierarchyDataAccessContext>();
-            optionBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HierarchyDataAccessControl");
-            var context = new HierarchyDataAccessContext(optionBuilder.Options);
-
-            // Act
+            // Arrange + Act
             IEnumerable<HierarchyNodeType> types = context.GetAllTypes();
 
             // Assert
