d415deec1222bdf2c5d0b2e55959f0f0099e0147
elias-ap <elias.ap.pro@gmail.com>
2025-06-20 13:19:32 -0300

    remove hierarchy third level

---
+++ src/HierarchyDataAccessControl.Data/Migrations/20250620161340_remove_hierarchy_third_level.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HierarchyDataAccessControl.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_hierarchy_third_level : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+                .DeleteData(
+                "HierarchyNodeTypes",
+                "Id",
+                4);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "HierarchyNodeTypes",
+                columns: ["Id", "Description"],
+                values: new object[,]
+                {
+                    { 4, "Employee" }
+                });
+        }
+    }
+}
